eea1e04e597aed58e0a4d5938682fdfde7e18ded
Camael24 <thehawk@live.fr>
2013-10-22 10:49:03 +0200

    Update file structure to last commit on Framework

---
+++ Application/Controller/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Application\Controller {
+    use Hoa\Dispatcher\Kit;
+
+    class Main extends Kit
+    {
+
+        public function IndexAction()
+        {
+            echo 'Bouya';
+        }
+    }
+}
